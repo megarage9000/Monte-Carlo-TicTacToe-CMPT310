63d3ad7a721d42c50669b54b4fdf947f8f8cdb41
jordoyo <john_ordoyo@sfu.ca>
2020-03-04 23:55:11 -0800

    TicTacToe game strucutre made

--- a3.py
+++ a3.py
@@ -1,28 +1,135 @@
 
 class Player:
-    def __init__(self, playerToken):
+    def __init__(self, playerToken, playerName):
         self.playerToken = playerToken
+        self.moveRow = self.moveColumn = 0
+        self.playerName = playerName
         
     def getPlayerToken(self):
         return self.playerToken
     
+    def getMove(self, moveList):
+        raise NotImplementedError("No move method yet")
     
+    def getPlayerName(self):
+        return self.playerName
+    
+class HumanPlayer(Player):
+    
+    def __init__(self, playerToken, playerName):
+        super().__init__(playerToken, playerName)
+        
+    def getMove(self, moveList):
+        isValidMove = False
+        while(not isValidMove):
+            try:
+                print("Enter row and column position of your move:")
+                self.moveRow = input("ROW >")
+                self.moveColumn = input("COLUMN >")
+                
+                # Checks if given move is one of the available moves
+                move = (self.moveRow, self.moveColumn)
+                for i in range(len(moveList)):
+                    if move == moveList[i]:
+                        isValidMove == True
+                        
+                if(isValidMove == False):
+                    print("Not a valid move!")
+
+            except TypeError:
+                print("Enter in integer please!")
+    
+        return self.moveRow, self.moveColumn
+                
 
 class Board: 
     
-    board = [['_' for x in range(3)] for x in range(3)]
-    
     def __init__(self):
+        self.emptyTile = '_'
+        self.board = [[self.emptyTile for x in range(3)] for x in range(3)]
+            
+    def setBoardPosition(self, token, row, column):  
+        self.board[row][column] = token
+    
+    def getBoardPosition(self, row, column):
+        return self.board[row][column]
+    
+    def isTileEmpty(self, row, column):
+        return self.board[row][column] == self.emptyTile
+    
+    # Getting connected rows for given token
+    def isConnectedinRow(self, token):        
+        for i in range(3):
+            # Horizontal rows
+            if(self.board[i][0] == token):
+                if(self.board[i][1] == self.board[i][2] == token):
+                    return True
+            
+            # Vertical rows
+            if(self.board[0][i] == token):
+                if(self.board[1][i] == self.board[2][i] == token):
+                    return True
+        
+        # Checking Angled rows        
+        if(self.board[0][0] == self.board[1][1] == self.board[2][2] == token):
+            return True
+        
+        elif(self.board[0][2] == self.board[1][1] == self.board[2][0] == token):
+            return True
+        
+        return False
+    
+    # Get all Empty Positions
+    def getEmptyPositions(self):
+        emptyPositions = []
+        for i in range(len(self.board)):
+            for k in range(len(self.board[i])):
+                if(self.isTileEmpty(i, k)):
+                    # Store move as tuple
+                    move = (i, k)
+                    emptyPositions.append(move)
+                    print("Found empty move!")
+        return emptyPositions
+    
+    def displayBoard(self):
+        for i in range(len(self.board)):
+            row = ""
+            for k in range(len(self.board[i])):
+                row += self.board[i][k] + " "
+            print(row)
+    
+class TicTacToe:
+    
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
+        self.board = Board()
         self.win = False
     
-    def getBoard(self):
-        return self.board
-    
-    
+    def playGame(self):
+        while(not self.win):
+            self.board.displayBoard()
+            self.playTurn(self.player1)
+            self.board.displayBoard()
+            self.playTurn(self.player2)
+            self.board.displayBoard()
+            
+    def playTurn(self, player):
+        if(self.win == False):
+            moveRow, moveColumn = player.getMove(self.board.getEmptyPositions())
+            self.board(player.getPlayerToken(), moveRow, moveColumn)
+            if(self.board.isConnectedinRow(player.getPlayerToken())):
+                self.win = True
+                winMessage = player.getPlayerName() + " Has won!"
+                print(winMessage)
         
+            
     
 def main():
-    print("Hello World")
+    player1 = HumanPlayer("O", "Player 1")
+    player2 = HumanPlayer("X", "Player 2")
+    game = TicTacToe(player1, player2)
+    game.playGame()
     
 
 
